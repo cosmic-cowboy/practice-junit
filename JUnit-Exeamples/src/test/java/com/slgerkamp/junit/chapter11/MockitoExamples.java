8bf1933e1de8fc5bda7a5c8e7798905cb0d196c5
hiroo takaku <slgerkamp@gmail.com>
2015-01-01 21:01:14 +0900

    chapter11 テストダブル：例外を送出するスタブメソッド

--- JUnit-Exeamples/src/test/java/com/slgerkamp/junit/chapter11/MockitoExamples.java
+++ JUnit-Exeamples/src/test/java/com/slgerkamp/junit/chapter11/MockitoExamples.java
@@ -28,4 +28,14 @@ public class MockitoExamples {
         // 検証
         assertThat(stub.get(0), is("Hello")); 
     }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void 例外を送出するスタブメソッド() throws Exception {
+        List<String> stub = mock(List.class);
+        when(stub.get(0)).thenReturn("Hello");
+        when(stub.get(1)).thenReturn("World");
+        when(stub.get(2)).thenThrow(new IndexOutOfBoundsException());
+        // 例外が送出される
+        stub.get(2); 
+    }
 }
