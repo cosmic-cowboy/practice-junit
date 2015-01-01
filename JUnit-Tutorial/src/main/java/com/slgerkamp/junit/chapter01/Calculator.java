0bd5bed91794760a6b6f534b4b43418d79598e73
hiroo takaku <slgerkamp@gmail.com>
2015-01-01 09:54:54 +0900

    chapter01 JUnitチュートリアル：テスト対象クラスとテストクラスの作成
    テスト対象クラス：計算処理
    テストクラス：テストケース（テストメソッド）未実装

---
+++ JUnit-Tutorial/src/main/java/com/slgerkamp/junit/chapter01/Calculator.java
@@ -0,0 +1,10 @@
+package com.slgerkamp.junit.chapter01;
+
+public class Calculator {
+	public int multiply(int x, int y){
+		return x * y;
+	}
+	public int divide(int x, int y){
+		return x / y;
+	}
+}
---
+++ JUnit-Tutorial/src/test/java/com/slgerkamp/junit/chapter01/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.slgerkamp.junit.chapter01;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("未実装");
+	}
+}
